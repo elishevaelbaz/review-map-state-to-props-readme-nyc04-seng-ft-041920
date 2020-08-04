ea5c5c54c2e86259b93cd7e0f9cacb7a7e1a646d
github username <github email address>
2020-08-03 22:13:06 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -6,18 +6,21 @@ import './App.css';
 class App extends Component {
 
   handleOnClickItems() {
-    this.props.store.dispatch({
+    this.props.dispatch({
       type: 'GET_COUNT_OF_ITEMS',
     });
   }
 
   handleOnClickUsers() {
-    this.props.store.dispatch({
+    this.props.dispatch({
       type: 'GET_COUNT_OF_USERS',
     });
   }
 
   render() {
+
+    console.log(this.props)
+
     // debugger;
     return (
       <div className="App">
@@ -34,7 +37,7 @@ class App extends Component {
 }
 
 const mapStateToProps = (state) => {
-  debugger;
+  // debugger;
   return { items: state.items }
 }
 
